442c34115efd9beca9d29f04828f2bbe65688bdb
Anthony mwaura <antony.mwaura@student.moringaschool.comc0deanthony@anthony-HP-EliteBook-840-G5:~/f11$ git config --global user.email antony.mwaura@student.moringaschool.com>
2024-10-01 15:35:04 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function superbowlWin(record) {
+    // Find the first record where the result is "W"
+    const winRecord = record.find(entry => entry.result === "W");
+  
+    if (winRecord) {
+      console.log(`The Denver Broncos won the Super Bowl in ${winRecord.year}`);
+      return winRecord.year;
+    } else {
+      console.log("The Denver Broncos did not win the Super Bowl.");
+      return undefined;
+    }
+  }
+  
+  module.exports = superbowlWin;
